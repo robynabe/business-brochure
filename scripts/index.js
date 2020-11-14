44d06a7c1a10d1529e5e6baf0a63432a822fa8d5
robynabe <64428921+robynabe@users.noreply.github.com>
2020-11-13 17:35:49 -0800

    Added leftside slide menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
-const body = document.body;
+/* const body = document.body;
 const btn = document.querySelector(".btn-menu");
 
 btn.addEventListener("click", function () {
   body.classList.toggle("show");
-});
+}); */
+
+function openSideMenu(){
+  document.getElementById("side-menu").style.width = "100%"
+}
+
+function closeSideMenu(){
+  document.getElementById("side-menu").style.width = "0"
+}
+
+  
