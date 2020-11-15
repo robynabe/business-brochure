6b40c26ff259f665b6221b498e8bc96fddf2db46
robynabe <64428921+robynabe@users.noreply.github.com>
2020-11-14 17:25:50 -0800

    Added style to social links

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,8 @@
-/* const body = document.body;
-const btn = document.querySelector(".btn-menu");
-
-btn.addEventListener("click", function () {
-  body.classList.toggle("show");
-}); */
-
-function openSideMenu(){
+function openSideMenu() {
   document.getElementById("side-menu").style.width = "100%"
 }
 
-function closeSideMenu(){
+function closeSideMenu() {
   document.getElementById("side-menu").style.width = "0"
 }
 
-  
