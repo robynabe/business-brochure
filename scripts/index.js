f5b85f01fc484dde19e45be0232afff7caed09d2
robynabe <64428921+robynabe@users.noreply.github.com>
2020-11-20 12:15:47 -0800

    Slick slide show working

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,27 @@ function closeSideMenu() {
   document.getElementById("side-menu").style.width = "0"
 }
 
+// Slick Slide Show //
+
+$(document).ready(() => {
+		$('#container-right .img').slick({
+      fade: true,
+      infinite: true,
+      autoplay: 4000,
+    })
+
+    /*$('.shop-device .room-device').slick({
+      responsive: [{
+        breakpoint: 500,
+        settings: {
+          speed: 2000,
+          infinite:true,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+
+      }]
+    })*/
+    
+});
+
