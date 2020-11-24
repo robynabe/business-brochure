e095680867abebf06b406689f06dacb967e78454
Seraphina White <seraphinawhite.6@gmail.com>
2020-11-23 19:41:55 -0800

    Final updates!

--- scripts/checkout.js
+++
@@ -1,7 +0,0 @@
-function showMessage(){
-    document.getElementById("thank-you").innerHTML = "Thank you for your order!"
-}
-
-var orderButton = document.getElementById("btn-order")
-orderButton.addEventListener("click", showMessage)
-
